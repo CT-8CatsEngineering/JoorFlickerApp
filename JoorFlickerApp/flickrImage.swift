583231f601feacda336d64a82c6d16ee6a452034
Colin <colin@LauraPeahiasMBP.fios-router.home>
2017-06-21 22:39:37 -0400

    first steps to persistence

--- JoorFlickerApp/flickrImage.swift
+++ JoorFlickerApp/flickrImage.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class FlickrImage: NSObject {
+class FlickrImage: NSObject, NSCoding {
     
     let imageID:String
     let titleString:String
@@ -17,6 +17,16 @@ class FlickrImage: NSObject {
     var previewImage:UIImage?
     let fullImagePath:String
     var fullImage:UIImage?
+
+    //MARK: types
+    struct PropertyKey {
+        static let imageID = "imageID"
+        static let title = "titleString"
+        static let previewImagePath = "previewImagePath"
+        static let previewImage = "previewImage"
+        static let fullImagePath = "fullImagePath"
+        static let fullImage = "fullImage"
+    }
     
     init(contents:[String:Any]) {
         
@@ -32,5 +42,32 @@ class FlickrImage: NSObject {
         
         super.init()
     }
+    init(id:String, title:String, previewPath:String, fullPath:String) {
+        imageID = id
+        titleString = title
+        previewImagePath = previewPath
+        fullImagePath = fullPath
+        
+        super.init()
+
+    }
     
+    //MARK: Coding functions
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(imageID, forKey: PropertyKey.imageID)
+        aCoder.encode(titleString, forKey: PropertyKey.title)
+        aCoder.encode(previewImagePath, forKey: PropertyKey.previewImagePath)
+        aCoder.encode(previewImage, forKey: PropertyKey.previewImage)
+        aCoder.encode(fullImagePath, forKey: PropertyKey.fullImagePath)
+        aCoder.encode(fullImage, forKey: PropertyKey.fullImage)
+    }
+    
+    required convenience init?(coder aDecoder:NSCoder) {
+        self.init(id:aDecoder.decodeObject(forKey: PropertyKey.imageID) as! String , title: aDecoder.decodeObject(forKey: PropertyKey.title) as! String, previewPath: aDecoder.decodeObject(forKey: PropertyKey.previewImagePath) as! String, fullPath: aDecoder.decodeObject(forKey: PropertyKey.fullImagePath) as! String)
+        
+        self.previewImage = (aDecoder.decodeObject(forKey: PropertyKey.previewImage) as! UIImage)
+        self.fullImage = (aDecoder.decodeObject(forKey: PropertyKey.fullImage) as! UIImage)
+        
+    }
+
 }
