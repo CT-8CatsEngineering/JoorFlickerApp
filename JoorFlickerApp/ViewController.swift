1898769ba451f6bde6462f0df73e51a5ab5dd5f2
Colin <colin@LauraPeahiasMBP.fios-router.home>
2017-06-23 09:51:22 -0400

    finished data persistence, when app closes normally. Added notification if the internet is disconnected, and cleaned up the scrolling on the pagination.

--- JoorFlickerApp/ViewController.swift
+++ JoorFlickerApp/ViewController.swift
@@ -17,25 +17,97 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     var data:[FlickrImage] = [FlickrImage]()
     var firstPage:Int = 0
     var lastPage:Int = 0
-    var totalPages:Int = 300//temp value for initial pagination testing
+    var totalImages:Int = 0
+    var totalPages:Int = 0
     var isLoading:Bool = false
-    var loadingPageNum:Int = 0
+    var loadingPageNum:Int = -1
     var loadingImages = [FlickrImage]()
-    
+    var searchString:String = ""
+    var isConnected:Bool = true
     
     @IBOutlet weak var imageTable: UITableView!
     @IBOutlet weak var flickerSearchBar: UISearchBar!
-    
+    @IBOutlet weak var AlertButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         flickerSearchBar.showsBookmarkButton = false
+        flickerSearchBar.text = searchString
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func saveResultsURL()->URL {
+        let fileManager = FileManager.default
+        let dirs = fileManager.urls(for: .documentDirectory, in: .userDomainMask).last
+        var filename = dirs
+        
+        filename?.appendPathComponent("lastFlickrSearchResult")
+        return filename!
+    }
+    
+    func saveResultsToFile() {
+        //create the dictionary of important information.
+        //data array, firstPage, LastPage, TotalPages, totalImages, searchBar.text
+        var saveDict:[String:Any] = [String:Any]()
+        saveDict["title"] = self.flickerSearchBar.text
+        saveDict["firstPage"] = self.firstPage
+        saveDict["lastPage"] = self.lastPage
+        saveDict["totalPages"] = self.totalPages
+        saveDict["totalImages"] = self.totalImages
+        saveDict["imageData"] = self.data
+        
+        //save file path
+        let filename = self.saveResultsURL()
+        let filePath:String = filename.path
+        
+        //save the contents of the dictionary
+        let success = NSKeyedArchiver.archiveRootObject(saveDict, toFile: filePath)
+        if !success {
+            print("failed to save the class to file.")
+        } else {
+            print("saved to file")
+        }
+
+    }
+    
+    func restoreResultsFromFile() {
+        let fileManager = FileManager.default
+        let fileURL = self.saveResultsURL()
+        if fileManager.fileExists(atPath: fileURL.path) {
+            let storedResultsDict:[String:Any] = NSKeyedUnarchiver.unarchiveObject(withFile: self.saveResultsURL().path) as! [String : Any]
+            if storedResultsDict.count == 0 {
+                fatalError("failed to unarchive the file into a valid dictionary")
+            } else {
+                //self.flickerSearchBar.text
+                self.searchString = storedResultsDict["title"] as! String
+                self.firstPage = storedResultsDict["firstPage"] as! Int
+                self.lastPage = storedResultsDict["lastPage"] as! Int
+                self.totalPages = storedResultsDict["totalPages"] as! Int
+                self.totalImages = storedResultsDict["totalImages"] as! Int
+                self.data = storedResultsDict["imageData"] as! [FlickrImage]
+                
+            }
+            
+        }
+    }
+    
+    func deleteLocalResultsStorage() {
+        let fileManager = FileManager.default
+        let fileURL = self.saveResultsURL()
+        if fileManager.fileExists(atPath: fileURL.path) {
+            do {
+                try fileManager.removeItem(at: fileURL)
+            } catch {
+                print("failed to remove the saveResults file")
+            }
+
+        }
+    }
+    
     @IBAction override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
         
@@ -44,7 +116,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             guard let fullImageController = navControl?.topViewController as? FullImageController else {
                 fatalError("Unexpected destination: \(segue.destination)")
             }
-            fullImageController.flickrImage = data[(imageTable.indexPathForSelectedRow?.row)!]
+            fullImageController.flickrImage = data[self.offsetArrayIndex(passedIndex: (imageTable.indexPathForSelectedRow?.row)!)]
         }
     }
     @IBAction func unwindFromPhoto(segue: UIStoryboardSegue) {
@@ -55,6 +127,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         flickerSearchBar.text = ""
         data = [FlickrImage]()
+        firstPage = 0
+        lastPage = 0
+        totalImages = 0
+        totalPages = 0
+        loadingPageNum = -1
+        isLoading = false
+        
+        self.deleteLocalResultsStorage()
+        
         imageTable.reloadData()
     }
     /*
@@ -66,14 +147,27 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             isLoading = true
             firstPage = 0
             lastPage = 0
+            totalImages = 0
             totalPages = 0
-            loadingPageNum = 0
+            loadingPageNum = -1
             
             self.loadNextPage()
             
             imageTable.reloadData()
         }
     }
+    
+    @IBAction func noInternetConnection(_ sender: Any) {
+        let alert = UIAlertController(title: "No Internet Connection", message: "We are unable to connect to the internet currently. You may continue to browse any previously loaded content.", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil ))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    func offsetArrayIndex(passedIndex:Int) -> Int {
+        return passedIndex - pageSize*firstPage
+    }
+    
     func loadNextPage() {
         self.loadPage(pageNumber: lastPage+1)
     }
@@ -85,11 +179,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let searchURL:URL = URL.init(string: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(flickrAPIKey)&tags=\(flickerSearchBar.text ?? "kittens")&page=\(pageNumber)&per_page=\(pageSize)&sort=date-posted-desc&format=json&nojsoncallback=1&media=photos")!
         let session = URLSession.init(configuration: URLSessionConfiguration.default, delegate: self, delegateQueue: nil)
         let sessionTask:URLSessionDownloadTask = session.downloadTask(with: searchURL, completionHandler: {(url:URL?,response:URLResponse?,error:Error?) in
-            if(error == nil){
+            if error == nil{
+                if !self.isConnected {
+                    self.isConnected = true
+                }
                 do {
                     let data:Data = try Data.init(contentsOf: url!)
                     let responseDictionary:Dictionary? = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as? [String: Any]
                     let photosDict = responseDictionary?["photos"] as? [String: Any]
+                    self.totalImages = Int(photosDict?["total"] as! String)!
                     self.totalPages = photosDict?["pages"] as! Int
                     self.loadingPageNum = photosDict?["page"] as! Int
                     let photosArray = photosDict?["photo"] as? [Any]
@@ -106,7 +204,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 } catch {
                     print("Error was nil but something failed in deserializing the data")
                 }
-            } else {
+            } else if (error as! URLError).code == URLError.notConnectedToInternet {
+                if self.isConnected {
+                    self.isConnected = false
+                    self.noInternetConnection(self)
+                }
+            }else {
                 print("error response when connecting to flicker: \(String(describing: error))" )
             }
         } )
@@ -134,50 +237,55 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         sessionTask.resume()
         
     }
-
-func downloadFullImage(session:URLSession, flickrImage:FlickrImage) {
-    let imageURL:URL = URL.init(string: flickrImage.fullImagePath)!
-    let sessionTask:URLSessionDownloadTask = session.downloadTask(with: imageURL, completionHandler: {(url:URL?,response:URLResponse?,error:Error?) in
-        
-        do {
-            if (error == nil){
-                let data:Data = try Data.init(contentsOf: url!)
-                let downloadedImage:UIImage = UIImage(data: data)!
-                flickrImage.fullImage = downloadedImage
-            } else {
-                print("error response when downloading file at URL: \(imageURL)")
-            }
-        } catch {
-            print("Error converting downloaded data into a UIImage or adding it to the contentImages array")
-        }
-    } )
-    sessionTask.resume()
     
-}
-
-
+    func downloadFullImage(session:URLSession, flickrImage:FlickrImage) {
+        let imageURL:URL = URL.init(string: flickrImage.fullImagePath)!
+        let sessionTask:URLSessionDownloadTask = session.downloadTask(with: imageURL, completionHandler: {(url:URL?,response:URLResponse?,error:Error?) in
+            
+            do {
+                if (error == nil){
+                    let data:Data = try Data.init(contentsOf: url!)
+                    let downloadedImage:UIImage = UIImage(data: data)!
+                    flickrImage.fullImage = downloadedImage
+                } else {
+                    print("error response when downloading file at URL: \(imageURL)")
+                }
+            } catch {
+                print("Error converting downloaded data into a UIImage or adding it to the contentImages array")
+            }
+        } )
+        sessionTask.resume()
+        
+    }
+    
+    
     // MARK: - Table View Delegate
-
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if isLoading {
-            return data.count+1
-        } else {
+        if data.count == 0 && !isLoading {
+            return 0
+        } else if data.count == 0 && isLoading {
+            return 1
+        }else if !isConnected {
             return data.count
+        } else {
+            return totalImages
         }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // if we hit an end of the data and we need to load more trigger that. (should already have been triggered but I have seen instances where it didn't)
-        if indexPath.row == data.count && lastPage != totalPages {
+        let index = self.offsetArrayIndex(passedIndex: indexPath.row)
+        if index >= data.count && lastPage != totalPages {
             if !isLoading {
                 isLoading = true
                 self.loadNextPage()
             }
-        }else if firstPage > 1 && indexPath.row == 0 {
+        }else if firstPage > 0 && indexPath.row <= pageSize*firstPage {
             if !isLoading {
                 isLoading = true
                 //load more into data here
@@ -185,19 +293,19 @@ func downloadFullImage(session:URLSession, flickrImage:FlickrImage) {
             }
         }
         
-        if indexPath.row == 0 && data.count == 0 && isLoading {
+        if index == 0 && data.count == 0 && isLoading {
             let cell = tableView.dequeueReusableCell(withIdentifier: "LoadingCell", for: indexPath)
             return cell
-        } else if indexPath.row == data.count && isLoading {
+        } else if index >= data.count && isLoading {
                 let cell = tableView.dequeueReusableCell(withIdentifier: "LoadingCell", for: indexPath)
                 return cell
-        } else if firstPage != 1 && indexPath.row == 0 && isLoading {
+        } else if firstPage != 0 && indexPath.row <= pageSize*firstPage && isLoading {
             let cell = tableView.dequeueReusableCell(withIdentifier: "LoadingCell", for: indexPath)
             return cell
         } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: "ImageInfoCell", for: indexPath) as! ImageInfoCell
-            cell.ImageNameLabel.text = data[indexPath.row].titleString
-            cell.ImagePreview.image = data[indexPath.row].previewImage
+            cell.ImageNameLabel.text = data[index].titleString
+            cell.ImagePreview.image = data[index].previewImage
             
             return cell
         }
@@ -208,13 +316,14 @@ func downloadFullImage(session:URLSession, flickrImage:FlickrImage) {
         return false
     }
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        if indexPath.row == data.count-pageSize && totalPages != lastPage {
+        let index = self.offsetArrayIndex(passedIndex: indexPath.row)
+        if index == data.count-pageSize && totalPages != lastPage {
             //load more into data here
             if !isLoading {
                 isLoading = true
                 self.loadNextPage()
             }
-        } else if firstPage != 1 && indexPath.row == pageSize {
+        } else if firstPage != 0 && index == pageSize {
             if !isLoading {
                 isLoading = true
                 //load more into data here
@@ -225,67 +334,43 @@ func downloadFullImage(session:URLSession, flickrImage:FlickrImage) {
     
     //MARK: URLSessionDelegate
     func urlSession(_ session: URLSession, didBecomeInvalidWithError error: Error?) {
-        if (error == nil) {
-            if loadingPageNum > lastPage { //loading the next page
-                var nextIndex:Int = data.count
-                let newLength:Int = data.count+loadingImages.count
-                var newIndices:[IndexPath] = [IndexPath]()
-                while nextIndex != newLength {
-                    newIndices.append(IndexPath.init(row: nextIndex, section: 0))
-                    nextIndex += 1
+        if isLoading {
+            performSelector(onMainThread: #selector(toggleAlertButton), with: nil, waitUntilDone: false)
+            if (error == nil) {
+                if loadingPageNum >= lastPage { //loading the next page
+                    performSelector(onMainThread: #selector(updateForNextPage), with: nil, waitUntilDone: false)
                     
-                }
-                performSelector(onMainThread: #selector(updateForNextPage(indices:)), with: newIndices, waitUntilDone: false)
-
-            } else if loadingPageNum < firstPage { //loading a page that we have already seen.
-                if firstPage == 1 {
+                } else if 0 <= loadingPageNum && loadingPageNum <= firstPage { //loading a page that we have already seen.
+                    performSelector(onMainThread: #selector(updateForPrevPage), with: nil, waitUntilDone: false)
                     
+                } else {
+                    performSelector(onMainThread: #selector(reloadData), with: nil, waitUntilDone: false)
+                    isLoading = false
                 }
-                var nextIndex:Int = 0
-                var newIndices:[IndexPath] = [IndexPath]()
-                while nextIndex != pageSize {
-                    newIndices.append(IndexPath.init(row: nextIndex, section: 0))
-                    nextIndex += 1
-                }
-                performSelector(onMainThread: #selector(updateForPrevPage(indices:)), with: newIndices, waitUntilDone: false)
-
+                
             } else {
-                //this would be bad we should only be loading pages that are outside our existing range.
-                print("loading page: (\(loadingPageNum)) is between FirstPage: \(firstPage) and lastPage: \(lastPage)")
-                isLoading = false
+                print("URLSession had an error: \(String(describing: error))")
+                self.isLoading = false
+                
             }
-            
-        } else {
-            print("URLSession had an error: \(error)")
         }
-        
     }
-
-    func updateForNextPage(indices:[IndexPath]) {
-        let visibleIndices = imageTable.indexPathsForVisibleRows
+    func reloadData() {
+        imageTable.reloadData()
+    }
+    func toggleAlertButton() {
+        AlertButton.isHidden = isConnected
+    }
+    
+    func updateForNextPage() {
         
         data.append(contentsOf: loadingImages)
-        imageTable.beginUpdates()
-        imageTable.insertRows(at: indices, with: UITableViewRowAnimation.bottom)
-        imageTable.endUpdates()
         
         if data.count > 200 {
             data = Array(data.dropFirst(pageSize))
-            var deletionIndices:[IndexPath] = [IndexPath]()
-            var i = 0
-            while i != pageSize {
-                deletionIndices.append(IndexPath.init(row: i, section: 0))
-                i += 1
-            }
-            imageTable.beginUpdates()
-            imageTable.deleteRows(at: deletionIndices, with: UITableViewRowAnimation.automatic)
-            imageTable.scrollToRow(at: IndexPath.init(row: (visibleIndices?.last?.row)!-pageSize, section: 0), at: UITableViewScrollPosition.bottom, animated: false)
-            imageTable.reloadData()
-            imageTable.endUpdates()
             firstPage += 1
-        } else {
-            imageTable.reloadData()
         }
+        imageTable.reloadData()
         
         lastPage += 1
         isLoading = false
@@ -293,14 +378,10 @@ func downloadFullImage(session:URLSession, flickrImage:FlickrImage) {
 
     }
     
-    func updateForPrevPage(indices:[IndexPath]) {
-        let visibleIndices = imageTable.indexPathsForVisibleRows
+    func updateForPrevPage() {
         loadingImages.append(contentsOf: data)
         data = loadingImages
-        imageTable.beginUpdates()
-        imageTable.insertRows(at: indices, with: UITableViewRowAnimation.top)
-        imageTable.endUpdates()
-        
+    
         if data.count > 200 {
             var i = data.count - pageSize
             var deletionIndices:[IndexPath] = [IndexPath]()
@@ -309,16 +390,10 @@ func downloadFullImage(session:URLSession, flickrImage:FlickrImage) {
                 i += 1
             }
             data = Array(data.dropLast(pageSize))
-            imageTable.beginUpdates()
-            imageTable.deleteRows(at: deletionIndices, with: UITableViewRowAnimation.bottom)
-            imageTable.scrollToRow(at: IndexPath.init(row: (visibleIndices?.last?.row)! + pageSize, section: 0), at: UITableViewScrollPosition.top, animated: false)
-            imageTable.reloadData()
-            imageTable.endUpdates()
-            
             lastPage -= 1
-        } else {
-            imageTable.reloadData()
         }
+        imageTable.reloadData()
+        
         firstPage -= 1
         isLoading = false
         loadingImages = [FlickrImage]()
